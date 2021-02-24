fbe7ff621e5f222d805ce7760254b1ecbcf2b85a
devilish21 <raghuveera098@gmail.com>
2021-02-24 12:57:41 +0530

    17

--- script.sh
+++ script.sh
@@ -1,18 +1,14 @@
 schema:
-	topic-key: test-topic1
-	compatability: forward
+	topic-key: test-topic1 compatability: forward
 	data: asfjhkjdfbksdgh/sd/f/gfd/hthththt/h/trh//hr
 schema:
-	topic-key: test-topic2
-	compatability: backward
+	topic-key: test-topic2 compatability: backward
 	data: asfjhkjdfbksdgh/sd/f/gfd/hthththt/h/trh//hr
 schema:
-	topic-key: test-topic3
-	compatability: backward
+	topic-key: test-topic3 compatability: backward
 	data: asfjhkjdfbksdgh/sd/f/gfd/hthththt/h/trh//dnbdsd
 schema:
-	topic-key: test-topic4
-	compatability: backward
+	topic-key: test-topic4 compatability: backward
 	data: asfjhkjdfbksdgh/sd/f/gfd/hthththt/h/trh//hr
 schema:
 	topic-key: test-topic5 compatability: backward
